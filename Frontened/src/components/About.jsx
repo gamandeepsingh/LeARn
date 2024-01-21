d88aa97bb57073ee467c7d3f09746c1339feed0f
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2024-01-21 21:36:21 +0530

    about page updated

--- Frontened/src/components/About.jsx
+++ Frontened/src/components/About.jsx
@@ -15,7 +15,26 @@ export default function About() {
             <h1 className='text-[8vh] font-[repo-extrabold] text-red-500 underline mt-2 smalling-text'>About leARn</h1>
             {/*  */}
             <div className='w-full about-text-container p-12 flex justify-evenly flex-wrap'>
-                <p className='w-[100%] sm:py-2 sm:w-[100%] md:w-[100%] lg:w-[50%] xl:w-[50%] text-gray-400 font-[repo-light] ' data-about-para>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ratione accusamus natus provident, vitae animi omnis veritatis quas! Eius officia sit exercitationem harum non neque ab laboriosam. Blanditiis voluptatem rerum tempore itaque nam mollitia necessitatibus est nemo iusto architecto! Tenetur deleniti aperiam rerum. Dicta voluptatem, nemo cupiditate quibusdam, natus error exercitationem </p>
+                <p className='w-[100%] sm:py-2 sm:w-[100%] md:w-[100%] lg:w-[50%] xl:w-[50%] text-gray-400 font-[repo-light] ' data-about-para>
+                leARn, developed by TEAM Cult for the Google Solution Challenge, revolutionizes education through innovative Augmented Reality (AR) technologies.
+                leARn bridges educational gaps, contributing to achieving United Nations Sustainable Development Goals.
+        </p>
+        <h2>Technology Stack</h2>
+        <p className='w-[100%] sm:py-2 sm:w-[100%] md:w-[100%] lg:w-[50%] xl:w-[50%] text-gray-400 font-[repo-light] ' data-about-para>
+            leARn utilizes cutting-edge technologies like OpenCV, deep learning, React, Django, and Google technologies to ensure a robust and efficient learning experience.
+        </p>
+
+        <h2>SDGs we address</h2>
+        <p className='w-[100%] sm:py-2 sm:w-[100%] md:w-[100%] lg:w-[50%] xl:w-[50%] text-gray-400 font-[repo-light] ' data-about-para>
+            leARn actively contributes to SDGs, including Quality Education (SDG 4), Industry, Innovation, and Infrastructure (SDG 9),
+            Reduced Inequalities (SDG 10), and Partnerships for the Goals (SDG 17).
+        </p>
+
+        <h2>Showstoppers</h2>
+        <p className='w-[100%] sm:py-2 sm:w-[100%] md:w-[100%] lg:w-[50%] xl:w-[50%] text-gray-400 font-[repo-light] ' data-about-para>
+            Innovative features include augmenting audio, video, 3D animation, and providing meanings of difficult words on each page,
+            enhancing the overall learning experience for every student.
+        </p>
                 <div className=' rounded-2xl object-contain overflow-hidden'>
                     <img src={gif} width="400px" height="100%" alt="" />
                 </div>
